fb6844122ed1c43978f347ae4210b4e8fc71dc77
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-10-30 16:14:25 +0500

    remove prefixes

--- 02_CLASS(STRING)/practice.py
+++ 02_CLASS(STRING)/practice.py
@@ -10,23 +10,31 @@
 # print(f"Title: {name.title()}")
 
 
-import re
-# Whitespaces
-name: str = "   Jawwad   Ahmad    "
+# import re
+# # Whitespaces
+# name: str = "   Jawwad   Ahmad    "
 
-# Removed from left side
-# lstrip() method of string is used
-print(f"Removed from left: '{name.lstrip()}'")
+# # Removed from left side
+# # lstrip() method of string is used
+# print(f"Removed from left: '{name.lstrip()}'")
 
-# Removed from right side
-# rstrip() method of string is used
-print(f"Removed from right: '{name.rstrip()}'")
+# # Removed from right side
+# # rstrip() method of string is used
+# print(f"Removed from right: '{name.rstrip()}'")
 
-# Removed from both sides
-# strip() method of string is used
-print(f"Removed from both sides: '{name.strip()}'")
+# # Removed from both sides
+# # strip() method of string is used
+# print(f"Removed from both sides: '{name.strip()}'")
 
-# Removed from any place
-# sub() method of re library is used to remove whitespaces
-name1: str = re.sub(' {2,100}', ' ', name)
-print(f"Removed from any place: {name1}")
+# # Removed from any place
+# # sub() method of re library is used to remove whitespaces
+# name1: str = re.sub(' {2,100}', ' ', name)
+# print(f"Removed from any place: {name1}")
+
+
+
+# Removing Prefixes
+nostarch_url = 'https://nostarch.com'
+print(f"Before Removing: {nostarch_url}")
+nostarch_url = nostarch_url.removeprefix('https://')
+print(f"After Removing: {nostarch_url}")
