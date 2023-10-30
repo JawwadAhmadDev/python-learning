6fd24131f8ef325932e50256a2f251af5e2b8918
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-10-30 16:08:46 +0500

    whitespaces handling

--- 02_CLASS(STRING)/practice.py
+++ 02_CLASS(STRING)/practice.py
@@ -2,10 +2,31 @@
 # print(methods)
 
 
-name : str = "jawwad ahmad"
-print(f"Capitalize: {name.capitalize()}")
-print(f"Casefold: {name.casefold()}")
-print(f"Lower: {name.lower()}")
-print(f"Upper: {name.upper()}")
-print(f"Title: {name.title()}")
+# name : str = "jawwad ahmad"
+# print(f"Capitalize: {name.capitalize()}")
+# print(f"Casefold: {name.casefold()}")
+# print(f"Lower: {name.lower()}")
+# print(f"Upper: {name.upper()}")
+# print(f"Title: {name.title()}")
 
+
+import re
+# Whitespaces
+name: str = "   Jawwad   Ahmad    "
+
+# Removed from left side
+# lstrip() method of string is used
+print(f"Removed from left: '{name.lstrip()}'")
+
+# Removed from right side
+# rstrip() method of string is used
+print(f"Removed from right: '{name.rstrip()}'")
+
+# Removed from both sides
+# strip() method of string is used
+print(f"Removed from both sides: '{name.strip()}'")
+
+# Removed from any place
+# sub() method of re library is used to remove whitespaces
+name1: str = re.sub(' {2,100}', ' ', name)
+print(f"Removed from any place: {name1}")
