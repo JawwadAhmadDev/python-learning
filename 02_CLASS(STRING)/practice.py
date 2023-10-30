ab68e35b39a068a81886ad0e6d6415ca5d5dcbe2
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-10-30 15:21:51 +0500

    2nd class practice

---
+++ 02_CLASS(STRING)/practice.py
@@ -0,0 +1,11 @@
+# methods = [i for i in dir(str) if "__" not in i]
+# print(methods)
+
+
+name : str = "jawwad ahmad"
+print(f"Capitalize: {name.capitalize()}")
+print(f"Casefold: {name.casefold()}")
+print(f"Lower: {name.lower()}")
+print(f"Upper: {name.upper()}")
+print(f"Title: {name.title()}")
+
