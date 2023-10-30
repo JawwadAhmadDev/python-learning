fbaf7c1b0a5f3ccce9a4431683a097142ee18d01
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-10-30 23:12:07 +0500

    problem 2.4 solved

---
+++ python_book_assignments/2.4/solution.py
@@ -0,0 +1,14 @@
+# 2-4. Name Cases: Use a variable to represent a person’s name, and then print
+# that person’s name in lowercase, uppercase, and title case.
+
+
+name: str = "HaMMad AhMad"
+
+# lower case
+print(f"Lower Case: {name.lower()}")
+
+# uppercase
+print(f"Uppercase: {name.upper()}")
+
+# title case
+print(f"Title Case: {name.title()}")
