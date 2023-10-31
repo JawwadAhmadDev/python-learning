1007a6c1460fa5c66ab178f9ddd361dd504f853d
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-10-31 11:49:15 +0500

    problem 2.10 solved

---
+++ python_book_assignments/2.10/solution.py
@@ -0,0 +1,8 @@
+# 2-10. Favorite Number: Use a variable to represent your favorite number. Then,
+# using that variable, create a message that reveals your favorite number. Print
+# that message
+
+favorite_number : int = 15
+message : str = f"My favourite number is {favorite_number}"
+
+print(message)
