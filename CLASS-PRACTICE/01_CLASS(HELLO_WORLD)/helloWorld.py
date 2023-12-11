0479bf5e0aa6d47047a014c715ffe71a8de85e7c
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-12-11 08:11:29 +0500

    new code added

--- CLASS-PRACTICE/01_CLASS(HELLO_WORLD)/helloWorld.py
+++ CLASS-PRACTICE/01_CLASS(HELLO_WORLD)/helloWorld.py
@@ -1,2 +1,11 @@
-print("Pakistan")
-print("Zinda abad")
+message = 10
+print("message")
+
+name = "jawwad"
+qualification = "ADP"
+print(qualification)
+
+age = 24
+print("age")
+gpa = 3.67
+print(gpa)
