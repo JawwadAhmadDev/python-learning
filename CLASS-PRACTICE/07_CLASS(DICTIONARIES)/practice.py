3ded8c5059fa1a7f80b72d4e9eaea3cf1d45780d
Jawwad Ahmad <109460336+JawwadAhmadDev@users.noreply.github.com>
2024-02-29 15:42:21 +0500

    dictionary folder created and one dictionary created

---
+++ CLASS-PRACTICE/07_CLASS(DICTIONARIES)/practice.py
@@ -0,0 +1,13 @@
+from typing import Dict, Union
+
+# custom types
+Key = str
+Value = Union[int] 
+
+my_dict: Dict[str, int] = {
+    "name" : "Jawwad Ahmad",
+    "father_name" : "Abdul Jabbar",
+    "age" : 26,
+    "height" : 1.80,
+    "married" : False,
+}
