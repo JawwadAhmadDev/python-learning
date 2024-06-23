c4158b6bab47490aebb4e0de5b34ddcd1a8f9ac6
Jawwad Ahmad <109460336+JawwadAhmadDev@users.noreply.github.com>
2024-06-23 16:19:07 +0500

    updated

--- CLASS-PRACTICE/07_CLASS(DICTIONARIES)/practice.py
+++ CLASS-PRACTICE/07_CLASS(DICTIONARIES)/practice.py
@@ -1,13 +1,21 @@
-from typing import Dict, Union
+# from typing import Dict, Union
 
-# custom types
-Key = str
-Value = Union[int] 
+# # custom types
+# Key = str
+# Value = Union[int] 
 
-my_dict: Dict[str, int] = {
-    "name" : "Jawwad Ahmad",
-    "father_name" : "Abdul Jabbar",
-    "age" : 26,
-    "height" : 1.80,
-    "married" : False,
-}
+# my_dict: Dict[str, int] = {
+#     "name" : "Jawwad Ahmad",
+#     "father_name" : "Abdul Jabbar",
+#     "age" : 26,
+#     "height" : 1.80,
+#     "married" : False,
+# }
+
+
+from scipy import stats
+speed = [10,10, 20, 20, 5, 5]
+
+x = stats.mode(speed)
+
+print(x)
