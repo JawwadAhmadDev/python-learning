16d2b51c37ff48f3add088cff4ebe65afdd7cd67
Jawwad Ahmad <109460336+JawwadAhmadDev@users.noreply.github.com>
2024-06-27 23:24:21 +0500

    while loop content added

--- CLASS-PRACTICE/08_CLASS(INPUT&WHILE_LOOP)/practice.py
+++ CLASS-PRACTICE/08_CLASS(INPUT&WHILE_LOOP)/practice.py
@@ -1,6 +1,4 @@
-number = input("Enter a number, and I'll tell you if it's even or odd: ")
-number = int(number)
-if number % 2 == 0:
-    print(f"\nThe number {number} is even.")
-else:
-    print(f"\nThe number {number} is odd.")
+import sys
+arguments: list = sys.argv
+print("Script Name:", arguments[0])
+print("First Argument:", arguments[1])
